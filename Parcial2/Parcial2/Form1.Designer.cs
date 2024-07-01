a6e2fbd4cd6924c6bda71f6abd35c88884ce9236
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-07-01 08:43:42 -0300

    init repo

---
+++ Parcial2/Parcial2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Registrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(188, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ver todos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ver todos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(223, 83);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 49);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Registrar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(223, 148);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 49);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Registrar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 209);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Parcial2/Parcial2/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Parcial2
+{
+    public partial class Form1 : Form
+    {
+        #region expediente 
+        int[] Nros = new int[1000];
+        int[] Dnis= new int[1000];
+        double[] Pagos = new double[1000];
+        int cont = 0;
+
+        void Registrar(int nro, int dni, double pago)
+        {
+            Nros[cont] = nro;
+            Dnis[cont] = dni;
+            Pagos[cont] = pago;
+            cont++;
+        }
+
+        double CalcularPromedio()
+        {
+            double sum = 0;
+            double promedio = 0;
+            for (int n = 0; n < cont; n++)
+            { 
+                sum += Pagos[n];
+            }
+            if(cont>0)
+                promedio=sum/sum;
+
+            return promedio;
+        }
+
+        int[] cpyNros = new int[1000];
+        int[] cpyDnis = new int[1000];
+        double[] cpyPagos= new double[1000];
+        int cpyCont = 0;
+
+        void CalcularMayPromedio()
+        {
+            cpyCont = 0;
+            double promedio = CalcularPromedio();
+
+            for (int n = 0; n < cont; n++)
+            {
+                if (Pagos[cont] > promedio)
+                {
+                    cpyNros[cpyCont] = Dnis[n];
+                    cpyDnis[cpyCont] = Nros[n];
+                    cpyPagos[cpyCont] = Pagos[n];
+                }
+            }
+        }
+
+        void Ordenar()
+        {
+
+            for (int n = 0; n < cont-1; n++)
+            {
+                for (int m = n + 1; m < cont; m++)
+                {
+                    if (Pagos[n] < Pagos[m])
+                    {
+                        int nro = Nros[n];
+                        Nros[n] = Nros[m];
+                        Nros[m] = nro;
+
+                        int dni= Dnis[n];
+                        Dnis[n] = Dnis[m];
+                        Dnis[m] = dni;
+
+                        double pago = Pagos[n];
+                        Pagos[n] = Pagos[m];
+                        Pagos[m] = pago;
+                    }
+                }
+            }
+        }
+
+        int BuscarPorNro(int nro)
+        {
+            int idx = -1;
+            int n = 0;
+
+            while ( n < cont && idx==-1)
+            {
+                if (Nros[n]==nro)
+                {
+                    idx = n;
+                }
+                n++;
+            }
+            return idx;
+        }
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormDatos formDatos = new FormDatos();
+         
+            while (formDatos.ShowDialog() == DialogResult.OK)
+            {
+                int nro = Convert.ToInt32(formDatos.textBox1.Text);
+                int dni = Convert.ToInt32(formDatos.textBox2.Text);
+                double pago = Convert.ToDouble(formDatos.textBox3.Text);
+
+                Registrar(nro,dni, pago);
+
+                Text = cont.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormVer formVer = new FormVer();
+            Ordenar();
+
+            for (int n = 0; n < cont; n++)
+            {
+                int dni = Dnis[n];
+                int nro = Nros[n];
+                double pago = Pagos[n];
+                formVer.listBox1.Items.Add($"{nro} - {dni} - {pago}");
+            }
+
+            formVer.ShowDialog();
+
+            formVer.Dispose();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormVer formVer=   new FormVer();
+            /*
+            double promedio = CalcularPromedio();
+
+            for (int n = 0; n < cont; n++)
+            {
+                if (Pagos[cont] > promedio)
+                {
+                    int dni = Dnis[n];
+                    int nro = Nros[n];
+                    double pago = Pagos[n];
+                    formVer.listBox1.Items.Add($"{nro} - {dni} - {pago}");
+                }
+            }
+            **/
+
+            for (int n = 0; n < cpyCont; n++)
+            {
+                int dni = cpyDnis[n];
+                int nro = cpyNros[n];
+                double pago = cpyPagos[n];
+                formVer.listBox1.Items.Add($"{nro} - {dni} - {pago}");
+            }
+            formVer.ShowDialog();
+
+            formVer.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            FormVer formVer = new FormVer();
+
+            int dni = Convert.ToInt32(textBox1.Text);
+
+            int m = 0;
+            for (int n = 0; n < cont; n++)
+            {
+                int dni1 = Dnis[n];
+                if (dni1== dni)
+                {
+                    int nro = Nros[n];
+                    double pago = Pagos[n];
+                    formVer.listBox1.Items.Add($"{nro} - {dni} - {pago}");
+                    m++;
+                }
+            }
+            if (m == 0)
+            {
+                formVer.listBox1.Items.Add("No se encontraron expedientes");
+            }
+
+            formVer.ShowDialog();
+
+            formVer.Dispose();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int nro = Convert.ToInt32(textBox2.Text);
+            int idx = BuscarPorNro(nro);
+
+            FormDatos formDatos = new FormDatos();
+
+            if (idx > -1)
+            {
+                formDatos.textBox1.Text = Nros[idx].ToString();
+                formDatos.textBox2.Text = Dnis[idx].ToString();
+                formDatos.textBox3.Text = Pagos[idx].ToString("0.00");
+            }
+            else
+            {
+                MessageBox.Show("Registro no encontrado");
+            }
+            formDatos.ShowDialog();
+        }
+    }
+}
